4fa0801f674dbd273961217ee26cd9d86bcc1514
mvuthegoat <mvu5803@gmail.com>
2024-11-19 02:11:42 -0500

    add function to fetch videos filtered by cryptoAddress

--- src/services/videoData.ts
+++ src/services/videoData.ts
@@ -12,43 +12,16 @@ export async function fetchDiscoveryFeed(
   return response.json();
 }
 
-// Fetch meme-specific videos
-// export async function fetchMemeNameVideos(
-//   memeName: string,
-//   page: number = 1,
-//   limit: number = 10,
-// ): Promise<Video[]> {
-//   const response = await fetch(`/api/meme/${memeName}?page=${page}&limit=${limit}`);
-//   if (!response.ok) {
-//     throw new Error("Failed to fetch collection videos");
-//   }
-//   return response.json();
-// }
-// export async function fetchMemeNameVideos(
-//     memeName: string,
-//     page: number = 1,
-//     limit: number = 10
-//   ): Promise<Video[]> {
-//     const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
-//     const response = await fetch(`${baseUrl}/api/meme/${memeName}?page=${page}&limit=${limit}`);
-
-//     if (!response.ok) {
-//       throw new Error("Failed to fetch collection videos");
-//     }
-
-//     return response.json();
-//   }
-
-export async function fetchMemeNameVideos(
-  memeName: string,
+export async function fetchCryptoAddressVideos(
+  crypto_address: string,
   page: number = 1,
   limit: number = 10
 ): Promise<Video[]> {
   try {
     const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
-    const encodedMemeName = encodeURIComponent(memeName);
+    const encodedCA = encodeURIComponent(crypto_address);
     const response = await fetch(
-      `${baseUrl}/api/meme/${encodedMemeName}?page=${page}&limit=${limit}`,
+      `${baseUrl}/api/meme/${encodedCA}?page=${page}&limit=${limit}`,
       {
         // Add cache options
         cache: "no-store", // or 'force-cache' if you want to cache the results
