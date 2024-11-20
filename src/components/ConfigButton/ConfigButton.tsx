5d5f054c5a72deb9e572b14c93e3d3ce0149bfd9
mvuthegoat <mvu5803@gmail.com>
2024-11-20 01:12:35 -0500

    make 10s generation unavailable for now

--- src/components/ConfigButton/ConfigButton.tsx
+++ src/components/ConfigButton/ConfigButton.tsx
@@ -1,29 +1,24 @@
-import React from "react";
-import styles from "./ConfigButton.module.css";
+import React from 'react';
+import styles from './ConfigButton.module.css';
 
 interface ConfigButtonProps {
-  duration: number;
-  setDuration: (duration: number) => void;
+    duration: number;
+    setDuration: (duration: number) => void;
 }
 
-const ConfigButton: React.FC<ConfigButtonProps> = ({
-  duration,
-  setDuration,
-}) => {
-  return (
-    <div className={styles.configButton}>
-      <label>Duration: </label>
-      <select
-        value={duration}
-        onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
-          setDuration(parseInt(e.target.value))
-        }
-      >
-        <option value={5}>5 seconds</option>
-        <option value={10}>10 seconds</option>
-      </select>
-    </div>
-  );
+const ConfigButton: React.FC<ConfigButtonProps> = ({ duration, setDuration }) => {
+    return (
+        <div className={styles.configButton}>
+            <label>Duration: </label>
+            <select 
+                value={duration} 
+                onChange={(e: React.ChangeEvent<HTMLSelectElement>) => setDuration(parseInt(e.target.value))}
+            >
+                <option value={5}>5 seconds</option>
+                <option value={10} disabled>10 seconds</option>
+            </select>
+        </div>
+    );
 };
 
 export default ConfigButton;
