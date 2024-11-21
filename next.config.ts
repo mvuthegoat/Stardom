1988b15ebf20459cfaf0c1a74c9151ed10f9b9ce
mvuthegoat <mvu5803@gmail.com>
2024-11-21 05:15:56 -0500

    package updates

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   images: {
-    domains: [(process.env.NEXT_PUBLIC_CLOUDFRONT_URL ?? '').replace(/^https?:\/\//, '')], // Ensure the domain is valid
+    domains: [
+      (process.env.NEXT_PUBLIC_CLOUDFRONT_URL ?? "").replace(
+        /^https?:\/\//,
+        ""
+      ),
+      "img-v1.raydium.io", // Add Raydium's image domain
+      "coin-images.coingecko.com", // Add CoinGecko's image domain
+    ],
   },
 };
 
 export default nextConfig;
-
