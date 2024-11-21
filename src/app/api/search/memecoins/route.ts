b17f4f272274aa04a218d366a8cad7f8423c5016
mvuthegoat <mvu5803@gmail.com>
2024-11-21 05:29:31 -0500

    modify SQL query in search/memecoins to get distinct crypto_address with similar meme_origin

--- src/app/api/search/memecoins/route.ts
+++ src/app/api/search/memecoins/route.ts
@@ -37,56 +37,67 @@ export async function GET(req: Request) {
         { status: 400 }
       );
     }
-
-    // Execute the database query
-    // const result = await pool.query(
-    //   `SELECT meme_origin, COUNT(*) AS total_videos, SUM(likes) AS total_likes, MAX(created_at) AS latest_video_date
-    //     FROM stardom_videos
-    //     WHERE meme_origin ILIKE $1
-    //     GROUP BY meme_origin
-    //     ORDER BY total_likes DESC
-    //     LIMIT $2 OFFSET $3`,
-    //   [`%${query}%`, limit, offset]
-    // );
-    // BIG QUERY FOR: all meme_origin names are unified into the earliest meme_origin name for a given crypto_address 
+    // BIG QUERY FOR: all meme_origin names are unified into the earliest meme_origin name for a given crypto_address
     // Plus include any original_image_key for each meme_origin
     // This is just a short-term solution to display the profile pic of every memecoin
+    // const result = await pool.query(
+    //   `
+    //       WITH unified_meme_origin AS (
+    //         SELECT DISTINCT ON (crypto_address)
+    //           crypto_address,
+    //           meme_origin
+    //         FROM
+    //           stardom_videos
+    //         ORDER BY
+    //           crypto_address,
+    //           created_at ASC
+    //       )
+    //       SELECT
+    //           u.meme_origin,
+    //           sv.crypto_address,
+    //           COUNT(*) AS total_videos,
+    //           SUM(sv.likes) AS total_likes,
+    //           MAX(sv.created_at) AS latest_video_date,
+    //           MAX(sv.original_image_key) AS original_image_key
+    //       FROM
+    //           stardom_videos sv
+    //       JOIN
+    //           unified_meme_origin u
+    //         ON
+    //           sv.crypto_address = u.crypto_address
+    //       WHERE
+    //           u.meme_origin ILIKE $1
+    //       GROUP BY
+    //           u.meme_origin, sv.crypto_address
+    //       ORDER BY
+    //           u.meme_origin, total_likes DESC
+    //       LIMIT $2 OFFSET $3;
+    //     `,
+    //   [`%${query}%`, limit, offset]
+    // );
+
+    // Get distinct crypto_address from a search, then order by most created
     const result = await pool.query(
       `
-          WITH unified_meme_origin AS (
-            SELECT DISTINCT ON (crypto_address)
-              crypto_address,
-              meme_origin
-            FROM
-              stardom_videos
-            ORDER BY
-              crypto_address,
-              created_at ASC
-          )
-          SELECT 
-              u.meme_origin, 
-              sv.crypto_address, 
-              COUNT(*) AS total_videos, 
-              SUM(sv.likes) AS total_likes, 
-              MAX(sv.created_at) AS latest_video_date, 
-              MAX(sv.original_image_key) AS original_image_key
-          FROM 
-              stardom_videos sv
-          JOIN 
-              unified_meme_origin u
-            ON 
-              sv.crypto_address = u.crypto_address
-          WHERE 
-              u.meme_origin ILIKE $1
-          GROUP BY 
-              u.meme_origin, sv.crypto_address
-          ORDER BY 
-              u.meme_origin, total_likes DESC
-          LIMIT $2 OFFSET $3;
+            SELECT 
+                crypto_address, 
+                COUNT(*) AS total_videos,
+                SUM(likes) AS total_likes,
+                MAX(created_at) AS latest_video_date
+            FROM 
+                stardom_videos
+            WHERE 
+                meme_origin ILIKE $1
+            GROUP BY 
+                crypto_address
+            ORDER BY 
+                total_videos DESC
+            LIMIT $2 OFFSET $3;
         `,
       [`%${query}%`, limit, offset]
     );
-    console.log("RESULTS:", result);
+
+    // console.log("RESULTS:", result);
     // Return paginated response
     return NextResponse.json({
       message: "Success",
