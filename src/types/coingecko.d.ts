06f2949c0a5c26f2716b62ac8f6ac176a5c410c9
mvuthegoat <mvu5803@gmail.com>
2024-11-21 05:22:57 -0500

    add chart widget from CoinGecko in /meme

---
+++ src/types/coingecko.d.ts
@@ -0,0 +1,11 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    "gecko-coin-price-chart-widget": {
+      locale?: string;
+      "dark-mode"?: string;
+      outlined?: string;
+      "coin-id"?: string;
+      "initial-currency"?: string;
+    };
+  }
+}
