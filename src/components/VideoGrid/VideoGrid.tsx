72828c3723289e58f8e8b022f336eb32a81ba2a5
mvuthegoat <mvu5803@gmail.com>
2024-11-27 15:43:53 -0500

    improve designs for mobile browser (still pretty bad)

--- src/components/VideoGrid/VideoGrid.tsx
+++ src/components/VideoGrid/VideoGrid.tsx
@@ -17,7 +17,14 @@ const VideoGrid: React.FC<VideoGridProps> = ({ videos = [], title }) => {
     <div className={styles.gridContainer}>
       {title && <h2 className={styles.gridTitle}>{title}</h2>}
       <Masonry
-        breakpointCols={{ default: 5, 1600: 4, 1200: 3, 900: 2, 600: 1 }}
+        breakpointCols={{
+          default: 5,
+          1600: 4,
+          1200: 3,
+          900: 2,
+          600: 2,
+          400: 2, // Mobile portrait
+        }}
         className={styles.videoGrid}
         columnClassName={styles.videoColumn}
       >
