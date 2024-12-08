e499b32b6ead4adf22f3b5a770c991c4a0361650
mvuthegoat <mvu5803@gmail.com>
2024-12-08 03:04:35 -0500

    change Create Fun tabs order

---
+++ src/app/create-fun/instant-meme-maker/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import CreateFunV2 from "../../../components/CreateFun/CreateFunV2";
+
+const CreateFunPage: React.FC = () => {
+  return <CreateFunV2 />;
+};
+
+export default CreateFunPage;
